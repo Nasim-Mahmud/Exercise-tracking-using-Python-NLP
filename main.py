f0d46911fb83c076035e5ecc985c524aa21e9622
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-07-05 21:54:41 +0600

    Initializing NUTRITIONIX API_ID and API_KEY.

--- main.py
+++ main.py
@@ -16,5 +16,6 @@ nutrition_parameters = {
     "age": 26
 }
 
-response = requests.post(url="https://trackapi.nutritionix.com/v2/natural/exercise",json=nutrition_parameters, headers=secure_header)
+response = requests.post(url="https://trackapi.nutritionix.com/v2/natural/exercise", json=nutrition_parameters,
+                         headers=secure_header)
 print(response.text)
