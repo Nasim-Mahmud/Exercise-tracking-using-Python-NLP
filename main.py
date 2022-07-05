3110770500e1bd026da1f534887d11849ed5ef70
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-07-05 21:43:31 +0600

    Initializing NUTRITIONIX API_ID and API_KEY.

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import requests
 
 NUTRITIONIX_APP_ID = "928a9c3e"
 NUTRITIONIX_APP_KEY = "a0f0b15c97b41a3f83dd37514ca42a6d"
+
+secure_header = {
+    "x-app-id": NUTRITIONIX_APP_ID,
+    "x-app-key": NUTRITIONIX_APP_KEY
+}
+
