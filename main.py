a09d3a320f16ed5c28dc41f1d843c42708651f10
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-07-18 21:09:59 +0600

    Initializing NUTRITIONIX API_ID and API_KEY.

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ GENDER = "male"
 WEIGHT = 100.5
 HEIGHT = 177.64
 AGE = 26
-#setting parameters
+# setting parameters
 secure_header = {
     "x-app-id": NUTRITIONIX_APP_ID,
     "x-app-key": NUTRITIONIX_APP_KEY,
 }
 
-#Setting parameters
+# Setting parameters
 nutrition_parameters = {
     "query": input("Tell me which exercise you did?\n"),
     "gender": GENDER,
@@ -23,7 +23,6 @@ nutrition_parameters = {
 
 response = requests.post(url="https://trackapi.nutritionix.com/v2/natural/exercise", json=nutrition_parameters,
                          headers=secure_header)
-
 
 data = response.json()
 print(response.text)
