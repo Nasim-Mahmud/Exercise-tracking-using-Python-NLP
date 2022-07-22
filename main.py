44d83d0059679153bdac68bf28cb006e8c323e2a
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-07-22 15:32:39 +0600

    Adding sheety authorization.

--- main.py
+++ main.py
@@ -49,5 +49,5 @@ for exercise in data["exercises"]:
         }
     }
     sheety_response = requests.post(
-        url="https://api.sheety.co/75f004290b7a8e66b7e5741a6a9e4137/workoutTracking/workouts", json=inputs)
+        url="https://api.sheety.co/75f004290b7a8e66b7e5741a6a9e4137/workoutTracking/workouts", json=inputs, headers=sheety_headers)
     print(sheety_response.text)
